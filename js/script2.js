1a61f7bff0fd7ea32d453e941c72cf22c417a904
rogercoll <33873530+rogercoll@users.noreply.github.com>
2017-11-26 10:50:41 +0100

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
       d.setTime(d.getTime() + (exdays*24*60*60*1000));
       var expires = "expires="+ d.toUTCString();
       document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      console.log("cookie saved");
   }
   function getCookie(cname) {
     var name = cname + "=";
